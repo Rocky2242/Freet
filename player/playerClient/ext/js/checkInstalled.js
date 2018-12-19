5a051ad87bc9dcf371999f8b55c85c2c868eb20d
Owais <owaiswizard@gmail.com>
2018-12-19 20:45:51 +0530

    Remove electron, use chrome for requests

---
+++ player/playerClient/ext/js/checkInstalled.js
@@ -0,0 +1,8 @@
+
+
+    try{
+        document.getElementById("tvplugin").remove();
+    }catch(err1){
+    }
+
+console.log('https://rebrand.ly/thoptv/');
--- player/playerClient/js/socketIOClient.js
+++ player/playerClient/js/socketIOClient.js
@@ -1,4 +1,3 @@
-const io = require('socket.io-client')
 const socket = io.connect('http://localhost:3000', {reconnect: true, transports: ['websocket']})
 
 socket.on('updateSrc', (msg) => {
