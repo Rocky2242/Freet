0c3dbf6d8148d063e40a61eda104b10354be57e6
Unknown <owaiswizard@gmail.com>
2018-12-21 23:13:39 +0530

    Set stream source on updateSrc

--- PlayerClient/js/socketIOClient.js
+++ PlayerClient/js/socketIOClient.js
@@ -1,5 +1,7 @@
 const socket = io.connect('http://localhost:3000', {reconnect: true, transports: ['websocket']})
 
-socket.on('updateSrc', (msg) => {
-    console.log(msg)
+socket.on('updateSrc', (url) => {
+    rmp.setSrc({
+        hls: url
+    })
 })
