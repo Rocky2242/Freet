c74ac2a1d65687471f277e286f802901b630b4ee
Unknown <owaiswizard@gmail.com>
2018-12-23 01:47:45 +0530

    Add and configure drawer navigator with custom header

--- FreetApp/App.js
+++ FreetApp/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View, StatusBar, Platform } from 'react-native';
 import { AppLoading, Font } from 'expo';
-import { RkTheme } from 'react-native-ui-kitten';
+import { RkTheme, RkStyleSheet, RkButton, RkText } from 'react-native-ui-kitten';
+import { createStackNavigator, createDrawerNavigator, createAppContainer } from 'react-navigation';
+import { FontAwesome} from './app/assets/icons.js';
 import { DarkKittenTheme } from './app/config/darkTheme.js';
 import { bootstrap } from './app/config/bootstrap.js';
 import IndexView from './app/screens/index.js';
+import SettingsView from './app/screens/settings.js';
+import 'react-native-gesture-handler'; 
 
 bootstrap();
 
@@ -45,21 +49,62 @@ export default class App extends React.Component {
   }
 
   renderApp() {
+    const DrawerNavigator = createDrawerNavigator(
+      {
+        Home: IndexView,
+        Settings: SettingsView,
+      },
+      {
+        drawerBackgroundColor: DarkKittenTheme.colors.screen.base,
+        contentOptions: {
+          inactiveTintColor: 'white',
+        },
+      }
+    );
+
+    const StackNavigator = createStackNavigator(
+      {
+        Main: {
+          screen: DrawerNavigator,
+          navigationOptions: ({ navigation }) => ({
+            title: 'Freet',
+            headerTintColor: 'white',
+            headerLeft: (
+              <RkButton 
+                rkType='clear' 
+                style={{ 
+                  paddingHorizontal: 25, 
+                  paddingVertical: 15,
+                  marginRight: 15,
+                }}
+                onPress={navigation.openDrawer}>
+                <RkText rkType='header3 awesome'>{FontAwesome.bars}</RkText>
+              </RkButton>),
+            headerStyle: {
+              backgroundColor: DarkKittenTheme.colors.screen.base,
+              height: 80,
+            }
+          }),
+        }
+      }
+    );
+
+    const AppContainer = createAppContainer(StackNavigator);
+
     return (
       <View style={styles.container}>
-        <IndexView />
+        <AppContainer />
       </View>
-    );
-  }
+    )
+      }
 
-  render = () => (this.state.isLoaded ? this.renderApp() : this.renderLoading());
-}
+      render = () => (this.state.isLoaded ? this.renderApp() : this.renderLoading());
+    }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight,
-  },
-});
+  const styles = RkStyleSheet.create((theme) => ({
+    container: {
+      flex: 1,
+      // marginTop: Platform.OS === 'ios' ? 0 : StatusBar.currentHeight,
+      backgroundColor: theme.colors.screen.base,
+    },
+  }));
