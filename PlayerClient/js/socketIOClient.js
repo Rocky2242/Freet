94d0d943e484028f0c87bb0613c67ccce97d4f14
Unknown <owaiswizard@gmail.com>
2018-12-23 20:38:21 +0530

    Disable autoplay, call play on updateSrc

--- PlayerClient/js/socketIOClient.js
+++ PlayerClient/js/socketIOClient.js
@@ -3,5 +3,6 @@ const socket = io.connect('http://localhost:3000', {reconnect: true, transports:
 socket.on('updateSrc', (url) => {
     rmp.setSrc({
         hls: url
-    })
+    });
+    rmp.play();
 })
